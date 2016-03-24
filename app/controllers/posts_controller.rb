ff8f1239aedfd1b0d7b77370dd4ccb92373e0ef4
Yamazaki Asahi <b2w51134@shonan.bunkyo.ac.jp>
2016-03-25 01:08:39 +0900

    create_post_function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :post_params, only: [:create, :update]
+  # before_action :your_posts?, only: [:edit, :update, :destroy]
+  # before_action :authenticate_user!
+
   def index
     @posts = Post.all
   end
@@ -12,10 +15,10 @@ class PostsController < ApplicationController
     if user_signed_in?
      @post = Post.new
      # postモデルの空のオブジェクト
-   else
+    else
      redirect_to new_user_session_path
      # ユーザーがログインしていない場合ログイン画面にリダイレクト
-   end
+    end
   end
 
   def create
@@ -26,15 +29,25 @@ class PostsController < ApplicationController
    else
      render :new
    end
- end
+  end
 
  def edit
  end
 
  def update
+   @post.update(post_params)
+
+   if @post.save
+     redirect_to @post, notice: "変更が保存されました"
+   else
+     # 失敗した場合editに戻る
+     render :edit
+   end
  end
 
  def destroy
+   @post.destroy
+   redirect_to posts_path
  end
 
  private
@@ -44,6 +57,16 @@ class PostsController < ApplicationController
  end
 
  def post_params
-     params.require(:post).permit(:title, :icatch, :contents, :user_id)
+    params.require(:post).permit(:title, :icatch, :contents, :user_id)
  end
+
+#  def your_posts?
+#     @post = Post.find(params[:id])
+#     @user_id = @post.user_id
+#     if user_signed_in? && current_user.id != @user_id
+#       redirect_to :back, notice: "この投稿はあなたの投稿ではないため編集・削除できません。"
+#     elsif !user_signed_in?
+#       redirect_to new_user_session_path notice "ログインしてください"
+#     end
+#  end
 end
