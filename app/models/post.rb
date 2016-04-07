709bf81d290102f1c3891b9d43e257c0ab7e4c0e
Yamazaki Asahi <b2w51134@shonan.bunkyo.ac.jp>
2016-04-08 01:26:06 +0900

    fix conflict

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,1 @@
-class Post < ActiveRecord::Base
-  belongs_to :user
-  has_many :favorites
-  mount_uploader :image, ImageUploader
-  validates :user_id, presence: true
-  validates :title, presence: true
-  validates :contents, presence: true
-end
+
