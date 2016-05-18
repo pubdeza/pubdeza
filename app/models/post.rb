4b47fee0b8b3a5c544f059a10e94bd731076bb3f
Yamazaki Asahi <b2w51134@shonan.bunkyo.ac.jp>
2016-05-18 17:16:29 +0900

    modefied posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :favorites
-  has_many :comments
+  has_many :favorites, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   mount_uploader :image, ImageUploader
   # validates :user_id, presence: true
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   attr_accessor :login
-  has_many :posts
-  has_many :favorites
-  has_many :comments
+  has_many :posts, :dependent => :destroy
+  has_many :favorites, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   mount_uploader :image, ImageUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
