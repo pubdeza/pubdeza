d377de3fea4b0223215e0d427d5165da65639afa
Yamazaki Asahi <b2w51134@shonan.bunkyo.ac.jp>
2016-06-24 06:11:00 +0900

    お気に入りをlink_toに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508062345) do
+ActiveRecord::Schema.define(version: 20160623113838) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160508062345) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "image",      limit: 255
+    t.string   "prefecture", limit: 255
   end
 
   create_table "users", force: :cascade do |t|
